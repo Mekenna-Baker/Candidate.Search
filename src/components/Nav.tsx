ca36c2205153335cd974b8caa64c0fbab9fe2daf
mbaker <mekenna.drabner@gmail.com>
2024-09-06 14:54:17 -0600

    Updated .gitignore to ignore .env and removed .env.EXAMPLE

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,29 @@
+import { NavLink } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <ul className="nav">
+        <li>
+          {/* Creating Home navlink, and setting it as root/default page */}
+          <NavLink
+            to="/"
+            // Function assigning class name based on if the link is active //
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'}
+            // Navlink only becomes active when URL is "/". End of function //
+            end
+          >Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/SavedCandidates"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'}
+          >About</NavLink>
+        </li>
+      </ul>
+    </div>
   )
 };
 
